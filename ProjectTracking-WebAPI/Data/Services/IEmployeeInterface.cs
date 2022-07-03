342bf5e88f27d7075d266b0f38d4902ac717009b
HaseebAhmed49 <haseebahmed02@gmail.com>
2022-07-04 04:00:32 +0500

    Added IEmployeeInterface

---
+++ ProjectTracking-WebAPI/Data/Services/IEmployeeInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ProjectTracking_WebAPI.Models;
+
+namespace ProjectTracking_WebAPI.Data.Services
+{
+    public interface IEmployeeInterface
+    {
+        public void AddEmployee(Employee employee);
+
+        public List<Employee> GetAllEmployee();
+
+        public Employee GetEmployeeById(int id);
+    }
+}
+
