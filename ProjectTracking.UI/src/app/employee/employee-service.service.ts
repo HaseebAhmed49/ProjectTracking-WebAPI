a6c854ed63351b296e3c765ccc070c3c87a59931
HaseebAhmed49 <haseebahmed02@gmail.com>
2022-08-30 23:56:13 +0500

    Working on Employee CRUD operations

--- ProjectTracking.UI/src/app/employee/employee-service.service.ts
+++ ProjectTracking.UI/src/app/employee/employee-service.service.ts
@@ -54,7 +54,7 @@ export class EmployeeServiceService {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Methods': 'PUT',
+        'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT',
         'Authorization': 'Bearer '+ token,
       })
     };
